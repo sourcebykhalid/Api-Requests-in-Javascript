63ddd47ebb821d82530c8fdc9d9f1b03bc6e2af7
Khalid Shakeel <khalidshakeel46@gmail.com>
2023-07-20 10:36:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// XMHL request
+const requestUrl = "https://api.github.com/users/sourcebykhalid";
+const card = document.querySelector(".card");
+const anc = document.querySelector("#anc");
+const xhr = new XMLHttpRequest();
+xhr.open("GET", requestUrl);
+xhr.onreadystatechange = function () {
+  console.log(xhr.readyState);
+  if (xhr.readyState === 4) {
+    console.log(this.responseText);
+    const data = JSON.parse(this.response); // parse the response to a JavaScript object
+    card.innerHTML = data.bio;
+    anc.innerHTML = data.html_url;
+    console.log(data);
+    console.log(typeof data);
+    console.log(data.following);
+    console.log(data.bio);
+  }
+};
+xhr.send();
